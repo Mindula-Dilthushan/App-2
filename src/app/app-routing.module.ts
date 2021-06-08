1f2b1d4a5e2a939aa3e94512b7579be473b1b0ed
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-08 17:01:23 +0530

    router module to added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerPageComponent} from "./components/customer-page/customer-page.component";
+import {ItemPageComponent} from "./components/item-page/item-page.component";
+import {OrderPageComponent} from "./components/order-page/order-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'customer', component:CustomerPageComponent
+  },
+  {
+    path:'item', component:ItemPageComponent
+  },
+  {
+    path:'order', component:OrderPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
